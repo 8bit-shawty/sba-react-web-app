4eeb070679486597e7769b7eb1bb86e83ea08f1f
MF <mfregwork21@gmail.com>
2024-11-17 18:36:51 -0800

    Planet api connected

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Nav from './components/Nav'
 
 function App() {
 
-  const [category, setCategory] = useState("")
+  const [category, setCategory] = useState("people")
   const [id, setId] = useState(0)
 
   {/*instance of use navigate */}
@@ -16,9 +16,11 @@ function App() {
   const search = (e) => {
     e.preventDefault();
 
-    const cat = category;
-    const dataId = id;
-    navigate(`/${cat}/${dataId}`);
+    if(id){
+      navigate(`/${category}/${id}`)
+    } else{
+      alert("Please enter a valid id")
+    }
   }
 
 
@@ -28,14 +30,28 @@ function App() {
       <Nav/>
       {/*Good way to check if your variables are updating */}
       <div>
-        <h3>Choose a category</h3>
-      </div>
-      <div>
-        <Link to={<People/>}>
-          <button onClick={search}>People</button>
-        </Link>
-        <button onClick={search}>Planets</button>
-        <button onClick={search}>Starships</button>
+      <h3>Select a category and search with an ID</h3>
+        <form onSubmit={search}>
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="people">People</option>
+            <option value="planet">Planets</option>
+          </select>
+
+          <label htmlFor="id">ID:</label>
+          <input
+            type="number"
+            id="id"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+
+          <button type="submit">Search</button>
+        </form>
       </div>
         <Routes>
           <Route path={"/people/:id"} element={<People/>}></Route>
--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+
 const Planet = () => {
+    const {id} = useParams()
+
+    const [name, setName] = useState('')
+    const [population, setPopulation] = useState('')
+    const [terrain, setTerrain] = useState('')
+    const [diameter, setDiameter] = useState('')
+    const [climate, setClimate] =useState('')
+
+    const [err, setErr] = useState(false);
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/planets/${id}`)
+        .then(({data}) => {
+            setErr(false);
+            console.log(data)
+            const {name, population, terrain, diameter, climate} = data
+        })
+        .catch(({message}) => {
+            console.log(message)
+            setErr(true)
+        })
+    }, [id])
+
     return (
-        <p>These is a planet</p>
+        <>
+            {
+                err ? <p style={{color: 'red'}}>This planet is not in your quadrant...</p> 
+                : <></>
+            }
+            <h2>Planet Name: {name}</h2>
+            <p>id: {id}</p>
+            <p>Mass: {mass}</p>
+            <p>Height: {height}</p>
+            <p>Eyes: {eyes}</p>
+        </>
     )
 }
 
